0c97ed1686b4ee8f38a2a877eb2ffd13e25e417a
fra-2107 <timchimienti@gmail.com>
2024-06-17 20:36:41 +0200

    vy

--- js/arbres.js
+++ js/arbres.js
@@ -4,6 +4,7 @@ ajaxRequest('GET', 'php/request.php/arbres', afficheArbres);
 
 function afficheArbres(data){
     console.log("io");
+    data = JSON.stringify(data);
     let arbres = JSON.parse(data);
     console.log('non');
     let table = document.getElementById('tableArbres');
