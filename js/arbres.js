3a198137ac1bad871d02ae7c21960d7e4c812c05
fra-2107 <timchimienti@gmail.com>
2024-06-17 20:29:56 +0200

    bagyette

--- js/arbres.js
+++ js/arbres.js
@@ -1,9 +1,30 @@
 'use strict'
 
-helloWorld();
-ajaxRequest('GET', 'php/request.php/arbres', helloWorld);
+ajaxRequest('GET', 'php/request.php/arbres', afficheArbres);
 
-function helloWorld(data)
-{
-  console.log(data);
+function afficheArbres(data){
+    let arbres = JSON.parse(data);
+    let table = document.getElementById('tableArbres');
+    for (let arbre of arbres){
+        let tr = document.createElement('tr');
+        let td = document.createElement('td');
+        td.innerHTML = arbre.id;
+        tr.appendChild(td);
+        td = document.createElement('td');
+        td.innerHTML = arbre.nom;
+        tr.appendChild(td);
+        td = document.createElement('td');
+        td.innerHTML = arbre.age;
+        tr.appendChild(td);
+        td = document.createElement('td');
+        td.innerHTML = arbre.espece;
+        tr.appendChild(td);
+        td = document.createElement('td');
+        td.innerHTML = arbre.taille;
+        tr.appendChild(td);
+        td = document.createElement('td');
+        td.innerHTML = arbre.date_plantation;
+        tr.appendChild(td);
+        table.appendChild(tr);
+    }
 }
