fe50b0559bed55432e406f85046c4d9da64f13c3
fra-2107 <timchimienti@gmail.com>
2024-06-18 09:16:44 +0200

    test

--- js/arbres.js
+++ js/arbres.js
@@ -2,6 +2,10 @@
 
 ajaxRequest('GET', 'php/request.php/arbres', afficheArbres);
 
+$('#ajouterbtn').onclick(() => {
+    ajaxRequest('POST', 'php/request.php/arbres', afficheArbres);
+})
+
 function afficheArbres(data){
     console.log(data);
 }
