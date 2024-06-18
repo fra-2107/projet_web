42c4aea25c5f926f45dbb60945d29bf70a9ed9b7
fra-2107 <timchimienti@gmail.com>
2024-06-18 13:36:17 +0200

    de

--- js/arbres.js
+++ js/arbres.js
@@ -50,7 +50,7 @@ async function fetchOptionsFromDB(selectName) {
 
             // Ajouter les options récupérées au select
             data.forEach(optionData => {
-                console.log(optionData);
+                console.log(data);
                 let option = document.createElement('option');
                 option.value = optionData.id;
                 console.log(optionData.id);
