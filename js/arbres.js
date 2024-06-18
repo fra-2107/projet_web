280592bad74807b49992a9e3a01c4fb201ed2090
fra-2107 <timchimienti@gmail.com>
2024-06-18 10:13:12 +0200

    test

--- js/arbres.js
+++ js/arbres.js
@@ -1,6 +1,6 @@
 'use strict'
 
-ajaxRequest('GET', 'php/request.php/arbres', afficheArbres);
+// ajaxRequest('GET', 'php/request.php/arbres', afficheArbres);
 
 $('#ajoutArbre').submit((event) =>{
     event.preventDefault();
