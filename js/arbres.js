c1a700b0abd15cb53ce73d1e7451d47226e66376
fra-2107 <timchimienti@gmail.com>
2024-06-18 09:06:16 +0200

    de

--- js/arbres.js
+++ js/arbres.js
@@ -3,31 +3,5 @@
 ajaxRequest('GET', 'php/request.php/arbres', afficheArbres);
 
 function afficheArbres(data){
-    console.log("io");
-    let arbres = JSON.parse(data);
-    arbres = JSON.stringify(arbres);
-    console.log('non');
-    let table = document.getElementById('tableArbres');
-    for (let arbre of arbres){
-        let tr = document.createElement('tr');
-        let td = document.createElement('td');
-        td.innerHTML = arbre.id;
-        tr.appendChild(td);
-        td = document.createElement('td');
-        td.innerHTML = arbre.nom;
-        tr.appendChild(td);
-        td = document.createElement('td');
-        td.innerHTML = arbre.age;
-        tr.appendChild(td);
-        td = document.createElement('td');
-        td.innerHTML = arbre.espece;
-        tr.appendChild(td);
-        td = document.createElement('td');
-        td.innerHTML = arbre.taille;
-        tr.appendChild(td);
-        td = document.createElement('td');
-        td.innerHTML = arbre.date_plantation;
-        tr.appendChild(td);
-        table.appendChild(tr);
-    }
+    console.log(data);
 }
