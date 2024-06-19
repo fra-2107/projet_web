e2006ad6f6fdfce8876483abf9bf524f5fdb6f7a
fra-2107 <timchimienti@gmail.com>
2024-06-19 11:17:19 +0200

    fe

--- php/request.php
+++ php/request.php
@@ -116,7 +116,7 @@
 
         if (is_numeric($nb_clusters) && $nb_clusters > 0) {
             // Construction de la commande pour exécuter le script Python
-            $python_script = "../python/script_besoin_1.py"; // Chemin absolu vers le script Python
+            $python_script = "/var/www/etu0106/projet_web/python/script_besoin_1.py"; // Chemin absolu vers le script Python
             
             if (file_exists($python_script)) {
                 $command = "pyhton " . $python_script . " " . intval($nb_clusters);
