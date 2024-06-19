fde6eb824c5a7fd57221e2ae10139ee36b0ec2d6
fra-2107 <timchimienti@gmail.com>
2024-06-19 20:16:45 +0200

    ahh bah non

--- js/preds.js
+++ js/preds.js
@@ -20,17 +20,17 @@ ajaxRequest('GET', 'php/request.php/preds?age&id='+id, (response) => {
 });
 
 ajaxRequest('GET', 'php/request.php/preds?risque&id='+id, (response) => {
-    var jsonString = response;
+    // var jsonString = response;
 
-    // Convertir la chaîne JSON en un tableau d'objets JavaScript
-    var jsonData = JSON.parse(jsonString);
+    // // Convertir la chaîne JSON en un tableau d'objets JavaScript
+    // var jsonData = JSON.parse(jsonString);
 
-    // Accéder à la première (et unique, dans ce cas) entrée du tableau
-    var risqueEstim = jsonData[0].risque_estim;
+    // // Accéder à la première (et unique, dans ce cas) entrée du tableau
+    // var risqueEstim = jsonData[0].risque_estim;
 
     // Utiliser la valeur récupérée comme nécessaire
-    console.log("Valeur de risque_estim :", risqueEstim);
+    console.log("Valeur de risque_estim :", response);
 
     // Afficher le résultat
-    document.getElementById('valuerisque').innerHTML = risqueEstim;
+    document.getElementById('valuerisque').innerHTML = response;
 });
