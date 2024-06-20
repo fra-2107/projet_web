292fb8215a2a8d0be9acc7ca15579d3381eb4029
fra-2107 <timchimienti@gmail.com>
2024-06-20 19:45:55 +0200

    ded

--- js/tab.js
+++ js/tab.js
@@ -68,15 +68,24 @@ function afficheArbres(data) {
         td_btn.appendChild(predictionButton);
         tr.appendChild(td_btn);
 
-        let td_del = document.createElement('td');
-        let delButton = document.createElement('button');
-        delButton.textContent = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">        <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>      </svg>';
-        delButton.addEventListener('click', () => {
-            // Call the function for prediction
-            console.log('delete element : ' + el.id);
+        let svgButton = document.createElement('td');
+        let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+        svg.setAttribute("width", "16");
+        svg.setAttribute("height", "16");
+        svg.setAttribute("fill", "currentColor");
+        svg.setAttribute("class", "bi bi-trash3-fill");
+        svg.setAttribute("viewBox", "0 0 16 16");
+        svg.addEventListener('click', () => {
+            // Call the function when the SVG is clicked
+            console.log('SVG clicked');
+            console.log('Delete : ' + el.id);
         });
-        td_del.appendChild(delButton);
-        tr.appendChild(td_del);
+        let path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+        path.setAttribute("d", "M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5");
+        svg.appendChild(path);
+        svgButton.appendChild(svg);
+        tr.appendChild(svgButton);
 
         tableArbre.appendChild(tr);
     });
