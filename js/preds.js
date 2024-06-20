80079fcaacfb702a421dda1f5eef4e3b7c40c351
ewenosdu29 <ewen.helary@isen-ouest.yncrea.fr>
2024-06-20 13:57:53 +0200

    a

--- js/preds.js
+++ js/preds.js
@@ -22,31 +22,14 @@ ajaxRequest('GET', 'php/request.php/preds?age&id=' + id, (response) => {
 ajaxRequest('GET', 'php/request.php/preds?risque&id=' + id, (response) => {
     console.log('response' + response);
     
-    var map = L.map('map').setView([49.848, 3.287], 13);
-
-    // Ajouter les tuiles OpenStreetMap
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-
-    // Ajouter les marqueurs sur la carte
-    response.forEach(arbre => {
-        L.circle([arbre.longi, arbre.lat], {
-                color: 'green',
-                fillColor: '#3f0',
-                fillOpacity: 0.5,
-                radius: 5 // Rayon en mètres
-            }).addTo(map)
-            .bindPopup(`Arbre: ${arbre.lat}, ${arbre.longi}`);
-    });
-    
     if (response == 'false') {
         document.getElementById('valuerisque').innerHTML = "Pas de risque estimé pour cet arbre";
         return;
     }
-    else if(response == 'true') 
+    elseif(response == 'true') 
     {
         document.getElementById('valuerisque').innerHTML = "attention cet arbre peux tomber";
     }
 
 });
+
