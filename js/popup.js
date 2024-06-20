f6df3d4aba4b6da749c6ffb37c4ba70de351fc3b
ewenosdu29 <ewen.helary@isen-ouest.yncrea.fr>
2024-06-20 13:09:31 +0200

    a

--- js/popup.js
+++ js/popup.js
@@ -1,14 +1,15 @@
-console.log("Script chargé!");
-document.getElementById('ajoutArbre').addEventListener('submit', function(event) {
-    event.preventDefault(); // Empêche la soumission normale du formulaire
-    
-    const popup = document.getElementById('popup');
-    popup.classList.remove('hidden'); // Affiche la popup
-    
-    setTimeout(() => {
-        popup.classList.add('hidden'); // Cache la popup après 5 secondes
-        alert('Formulaire envoyé !'); // Affiche un message de confirmation (optionnel)
-        // Tu peux également soumettre le formulaire ici si nécessaire
-        // event.target.submit();
-    }, 5000);
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Script chargé!");
+    document.getElementById('ajoutArbre').addEventListener('submit', function(event) {
+        event.preventDefault(); // Empêche la soumission normale du formulaire
+
+        const popup = document.getElementById('popup');
+        popup.classList.remove('hidden'); // Affiche la popup
+
+        setTimeout(() => {
+            popup.classList.add('hidden'); // Cache la popup après 3 secondes
+            // Tu peux également soumettre le formulaire ici si nécessaire
+            // event.target.submit();
+        }, 3000);
+    });
 });
