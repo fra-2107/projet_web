1e57a4ec25e6de763b48a1a4d4f6bbb922fe60af
ewenosdu29 <ewen.helary@isen-ouest.yncrea.fr>
2024-06-20 14:01:56 +0200

    a

--- js/preds.js
+++ js/preds.js
@@ -36,7 +36,24 @@ ajaxRequest('GET', 'php/request.php/preds?risque&id=' + id, (response) => {
 ajaxRequest('GET', 'php/request.php/preds?map&id=' + id, (response) => {
     console.log('response' + response);
     
-    
+    // Initialiser la carte centrée sur Saint-Quentin
+    var map = L.map('map').setView([49.848, 3.287], 13);
+
+    // Ajouter les tuiles OpenStreetMap
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    // Ajouter les marqueurs sur la carte
+    response.forEach(arbre => {
+        L.circle([arbre.longi, arbre.lat], {
+                color: 'green',
+                fillColor: '#3f0',
+                fillOpacity: 0.5,
+                radius: 5 // Rayon en mètres
+            }).addTo(map)
+            .bindPopup(`Arbre: ${arbre.lat}, ${arbre.longi}`);
+    });
 
 });
 
