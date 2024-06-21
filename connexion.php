1f0b53a19ff8034881a0099ec50a237da8393524
ewenosdu29 <ewen.helary@isen-ouest.yncrea.fr>
2024-06-21 09:43:58 +0200

    a

--- connexion.php
+++ connexion.php
@@ -1,56 +1,35 @@
-<?php 
-// récupération de la connexion à la bdd
-require_once("php/database.php");
+<?php
+
+require_once("php/database.php"); // on recupere le fichier qui connecte la bdd
+
 $erreur = "";
-$erreur_mdp = "";
 
-// lorsqu'on clique sur le bouton
-if(isset($_POST["btn-create"])) {
+if(isset($_POST["check-btn"])) {
 
-    // on vérifie si tous les champs sont remplis
-    if(empty($_POST["nom"]) || empty($_POST["prenom"]) || empty($_POST["age"]) || empty($_POST["mail"]) || empty($_POST["mdp"]) || empty($_POST["confirm-mdp"])) {
-        $erreur = "Veuillez remplir tous les champs";
-    } else {
-        $nom = $_POST["nom"];
-        $prenom = $_POST["prenom"];
-        $age = $_POST["age"];
-        $mail = $_POST["mail"];
-        $mdp = $_POST["mdp"];
-        $confirm_mdp = $_POST["confirm-mdp"];
+    if(isset($_POST['email']) && isset($_POST['pwd'])) { // on verifie si l'utilisateur a bien rentré des donnée
 
-        // vérification que les mots de passe correspondent
-        if ($mdp != $confirm_mdp) {
-            $erreur_mdp = "Les mots de passe ne correspondent pas";
-        } elseif (!is_numeric($age) || intval($age) <= 0) {
-            $erreur = "L'âge doit être un nombre entier positif";
+        $email = $_POST['email'];
+        $pwd = $_POST['pwd'];
+
+        $con = dbConnect();
+
+        if(!$con) {
+            header("HTTP/1.1 503 Service Unavailable");
+            exit;
+        }
+
+        $request = $con->prepare("SELECT * FROM utilisateur WHERE mail=? AND mdp=?");
+        $request->bindParam(1, $email);
+        $request->bindParam(2, $pwd);
+        $request->execute();
+
+        $num_ligne = $request->rowCount(); // compter le nb de ligne ayant un rapport avec la requete sql
+        if ($num_ligne > 0) {
+            session_start();
+            $_SESSION['mail'] = $email;
+            header("Location:index_accueil.html");
         } else {
-            $age = intval($age); // convertir l'âge en entier
-
-            $con = dbConnect();
-
-            if(!$con) {
-                header("HTTP/1.1 503 Service Unavailable");
-                exit;
-            }
-
-            $request = $con->prepare("
-            INSERT INTO utilisateur (mail, nom, prenom, age, mdp)
-            VALUES (:email, :nom, :prenom, :age, :mdp)
-            ");
-
-            $request->bindParam(':email', $mail);
-            $request->bindParam(':nom', $nom);
-            $request->bindParam(':prenom', $prenom);
-            $request->bindParam(':age', $age);
-            $request->bindParam(':mdp', $mdp);
-
-            try {
-                $request->execute();
-                header("Location:index_connexion.php");
-                exit;
-            } catch (PDOException $e) {
-                $erreur = "Erreur lors de l'inscription : " . $e->getMessage();
-            }
+            $erreur = "Adresse mail ou mot de passe incorrect !";
         }
     }
 }
@@ -63,45 +42,42 @@ if(isset($_POST["btn-create"])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/style.css">
-    <title>Projet web - nouveau compte</title>
+    <title>Projet web - connexion</title>
 </head>
 <body>
-    <!-- Formulaire pour créer un nouveau compte -->
 
     <div class="page">
 
-        <h1>Créer un compte</h1>
-
+        <h1>Bienvenue</h1>
+        
         <div class="form">
-            <form class="form_auth_1" method="POST">
+            <form class="form_auth_1" method="POST" action="">
 
                 <div class="top_form">
-                    <?php
-                    // Gestion des erreurs
-                    if($erreur_mdp != "") {
-                        echo "<p class='erreur'>".$erreur_mdp."</p>";
-                    }
+                    <div class="logo"></div>
+                    <h2>Connectez-vous</h2>
 
-                    if($erreur != "") {
+                    <?php 
+
+                    //Gestion des erreurs
+                    if(isset($erreur)) {
                         echo "<p class='erreur'>".$erreur."</p>";
                     }
                     ?>
-                    <input type="text" placeholder="Nom" name="nom">
-                    <input type="text" placeholder="Prénom" name="prenom">
-                    <input type="text" placeholder="Date de naissance (exemple : 2023-06-06)" name="age">
-                    <input type="text" placeholder="Email" name="mail">
-                    <input type="password" placeholder="Mot de passe" name="mdp">
-                    <input type="password" placeholder="Confirmez mot de passe" name="confirm-mdp">
+
+                    <input type="text" placeholder="Email" name="email">
+                    <input type="text" placeholder="Mot de passe" name="pwd">
+                    <button  name="check-btn">Connexion</button>
                 </div>
 
-                <div class="bottom_form">
-                    <button class="btn-create" name="btn-create">S'inscrire</button>
-                </div>
-
+                <hr>
             </form>
+
+            <div class="bottom_form">
+                <p>Vous n'avez pas de compte ?</p>
+                <a href="create_account.php"><button class="btn-create">Créer un compte</button></a>
+            </div>
         </div>
     </div>
-
-    <!-- fin du formulaire -->
 </body>
 </html>
